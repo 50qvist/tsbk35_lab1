8849577799a194c58bb6008d2f6c1f0be3a844be
Törnqvist <jakob.tornqvist@gmail.com>
2022-01-31 18:01:15 +0100

    stuffs

--- sound.m
+++ sound.m
@@ -12,21 +12,38 @@ counts = zeros(length(values),1);
 for i=1:length(chosen_sound)
     counts(values==chosen_sound(i)) = counts(values==chosen_sound(i))+1;
 end
-
 %plot(counts)
-count_prob = counts / length(chosen_sound);
+counts_probs = counts / length(chosen_sound);
 %plot(probabilities);
 
-entropy = -sum(count_prob.*log2(count_prob),'omitnan')
+entropy = -sum(counts_probs.*log2(counts_probs),'omitnan')
 
 %====Parentropi H(Xi, Xi+1)=====
 
 no_pairs = length(chosen_sound)-1;
-counts_pairs = zeros(length(values),1);
+counts_pairs = zeros(length(values));
 
 for i = 1:no_pairs-1
-    i
     counts_pairs(values == chosen_sound(i), values == chosen_sound(i+1)) = counts_pairs(values == chosen_sound(i), values == chosen_sound(i+1)) +1;
 end
-    plot(count_pairs);
+    %size(counts_pairs)
+    %plot(counts_pairs);
+    
+count_pairs_prob = counts_pairs/(length(chosen_sound));
+
+
+entropy_pairs = -sum(count_pairs_prob(:).*log2(count_pairs_prob(:)),'omitnan')
+    
 %====Betingad entropi H(Xi+1 | Xi)======
+
+% H(Xi+1,Xi) = H(Xi, Xi+1) - H(Xi)
+
+entropy_conditional = entropy_pairs - entropy
+
+% ======================= HUFFMAN PART =====================
+
+huffman_memory_free = huffman(counts_probs)
+
+
+
+
